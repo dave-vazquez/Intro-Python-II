850a85b98783e53ab0439f29881012a53fe667f8
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-11 14:39:17 -0400

    adds self prefix, corrects player instantiation

--- src/player.py
+++ src/player.py
@@ -1,7 +1,13 @@
+from room import rooms
+
+
 class Player:
     def __init__(self, name, current_room):
         self.name = name
-        current_room = current_room
+        self.current_room = current_room
 
     def __str__(self):
         return f"{{ name: {self.name}, current_room: {self.current_room} }}"
+
+
+player = Player("hero", rooms["outside"])
