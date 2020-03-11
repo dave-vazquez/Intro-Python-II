17064243d25983e4c14aee038bb0e2b5ed364108
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-11 14:56:21 -0400

    implements basic REPL structure

--- src/adv.py
+++ src/adv.py
@@ -1,51 +1,70 @@
-from room import Room
+from PyInquirer import prompt
+from colorama import Fore
+from room import rooms
+from player import player
+import copy
+import sys
+import os
 
-# Declare all the rooms
-
-room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
-}
+# function to clear terminal after each input command
+def clear():
+    return os.system("clear")
 
 
-# Link rooms together
+clear()
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+"""
+    Main
 
-#
-# Main
-#
+    Make a new player object that is currently in the 'outside' room.
 
-# Make a new player object that is currently in the 'outside' room.
+    Write a loop that:
 
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+    * Prints the current room name
+    * Prints the current description (the textwrap module might be useful here).
+    * Waits for user input and decides what to do.
+
+    # Print an error message if the movement isn't allowed.
+    If the user enters a cardinal direction, attempt to move to the room there.
+
+    If the user enters "q", quit the game.
+"""
+
+cardinal_directions = [
+    {
+        "type": "list",
+        "name": "menu",
+        "message": "What direction do you want to move in?",
+        "choices": ["North", "South", "East", "West", "Quit"],
+    }
+]
+
+
+cmd = ""
+while not cmd == "Quit":
+    print(
+        Fore.CYAN
+        + f"Current Room: {player.current_room.name} \
+        \n{player.current_room.description}"
+    )
+
+    cmd = prompt(cardinal_directions)["menu"]
+
+    if cmd == "North":
+        clear()
+        print("Moves North")
+    elif cmd == "South":
+        clear()
+        print("Moves South")
+    elif cmd == "East":
+        clear()
+        print("Moves East")
+    elif cmd == "West":
+        clear()
+        print("Moves West")
+    else:
+        clear()
+        print("Invalid command. Try again.")
+clear()
+print("Your journey has ended. For now...")
+
