9971b0ed397d719c8a5c28726bd428e5c3b46f1e
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-11 16:54:35 -0400

    adds getters

---
+++ src/item.py
@@ -0,0 +1,13 @@
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def get_name(self):
+        return self.name
+
+    def get_description(self):
+        return self.description
+
+    def __str__(self):
+        return f" {{ name: {self.name}, description: {self.description} }}"
