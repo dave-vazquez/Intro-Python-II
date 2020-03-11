f33a9c8f39ae7e808d5d5dc5e1fa364c332a2055
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-11 15:52:06 -0400

    adds a move method to the Player object

--- src/player.py
+++ src/player.py
@@ -9,5 +9,15 @@ class Player:
     def __str__(self):
         return f"{{ name: {self.name}, current_room: {self.current_room} }}"
 
+    def move(self, direction):
+        if direction == "North":
+            self.current_room = self.current_room.n_to
+        elif direction == "South":
+            self.current_room = self.current_room.s_to
+        elif direction == "East":
+            self.current_room = self.current_room.e_to
+        elif direction == "West":
+            self.current_room = self.current_room.e_to
+
 
 player = Player("hero", rooms["outside"])
