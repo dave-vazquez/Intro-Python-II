7bc987a9d701e7cecda4599c42f9f4d302d209f2
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-12 16:30:59 -0400

    raises custom exception

--- src/player.py
+++ src/player.py
@@ -15,17 +15,23 @@ class Player:
         return self.items
 
     def move(self, direction):
-        if direction == "North":
+        if direction == "North" and self.current_room.n_to is not None:
             self.current_room = self.current_room.n_to
-        elif direction == "South":
+        elif direction == "South" and self.current_room.s_to is not None:
             self.current_room = self.current_room.s_to
-        elif direction == "East":
+        elif direction == "East" and self.current_room.e_to is not None:
             self.current_room = self.current_room.e_to
-        elif direction == "West":
-            self.current_room = self.current_room.e_to
+        elif direction == "West" and self.current_room.w_to is not None:
+            self.current_room = self.current_room.w_to
+        else:
+            raise InvalidMoveError()
 
     def __str__(self):
         return f"{{ name: {self.name}, current_room: {self.current_room} }}"
+
+
+class InvalidMoveError(Exception):
+    pass
 
 
 player = Player("hero", rooms["outside"])
